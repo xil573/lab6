4295ebb50cc9608f91bac6b385c97902a10ddcd2
Xindi LI <xil573@eng.ucsd.edu>
2017-02-16 12:44:03 -0800

    ajax lab6 xindi

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,12 +27,39 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/" + idNumber, callback);
+
+	console.log("/project/" + idNumber);
+
 }
 
+function callback(result){
+	console.log(result);
+	var projectHtml = 
+	'<img src = "' + result.image + ' " class = "detailsImage">' + 
+	'<p>' + result.title + '</p>' + 
+	'<p><small>' + result.date + '</small></p>' +
+	'<p>' + result.summary + '</p>'
+
+	$("#project" + result.id + " .details").html(projectHtml);
+
+}
 /*
  * Make an AJAX call to retrieve a color palette for the site
  * and apply it
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	e.preventDefault();
+	$.get("/palette", setColor);
+}
+
+function setColor(result){
+	var color = result.colors.hex;
+	$('body').css('background-color', color[0]);
+	$('.thumbnail').css('background-color', color[1]);
+	$('h1, h2, h3, h4').css('color', color[2]);
+	$('p').css('color', color[3]);
+	$('.project img').css('opacity', .80);
 }
